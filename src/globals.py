1dad3f143d82af75516db883ffb694ae38141c80
Fortunato Ventre <vorione@gmail.com>
2009-06-29 02:57:54 +0200

    * I forgot to update version string too.

--- src/globals.py
+++ src/globals.py
@@ -23,7 +23,7 @@ LOCALE_DIR = "/usr/share/locale/"
 DATA_IMG_DIR = os.path.join(DATA_DIR, "images")
 
 APP_NAME = "DsRomsManager"
-APP_VERSION = "0.4.1"
+APP_VERSION = "0.4.2"
 
 # gettext
 import locale, gettext
