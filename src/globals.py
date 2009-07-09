a9d68708684da0cb26933a201e4f117b3d94a0e2
Fortunato Ventre <vorione@gmail.com>
2009-07-10 01:18:48 +0200

    * Bump version to 1.0rc1. * Update changelog.

--- src/globals.py
+++ src/globals.py
@@ -23,7 +23,7 @@ LOCALE_DIR = "/usr/share/locale/"
 DATA_IMG_DIR = os.path.join(DATA_DIR, "images")
 
 APP_NAME = "DsRomsManager"
-APP_VERSION = "0.9.9"
+APP_VERSION = "1.0rc1"
 
 import gettext
 _ = gettext.gettext
