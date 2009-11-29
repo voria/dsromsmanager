f01579889c58e68b1fd78f60d2b0074311c1d55b
Fortunato Ventre <vorione@gmail.com>
2009-11-29 13:46:25 +0000

    Release 1.0.3.

--- src/globals.py
+++ src/globals.py
@@ -23,7 +23,7 @@ LOCALE_DIR = "/usr/share/locale/"
 DATA_IMG_DIR = os.path.join(DATA_DIR, "images")
 
 APP_NAME = "DsRomsManager"
-APP_VERSION = "1.0.2"
+APP_VERSION = "1.0.3"
 
 import gettext
 _ = gettext.gettext
