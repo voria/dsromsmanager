56779870cf89e3b8916d1ce5e37d49d409978865
Fortunato Ventre <vorione@gmail.com>
2013-11-15 12:30:16 +0100

    Bump version to 1.5, update debian and arch files

--- src/globals.py
+++ src/globals.py
@@ -23,7 +23,7 @@ LOCALE_DIR = "/usr/share/locale/"
 DATA_IMG_DIR = os.path.join(DATA_DIR, "images")
 
 APP_NAME = "DsRomsManager"
-APP_VERSION = "1.4"
+APP_VERSION = "1.5"
 
 import gettext
 _ = gettext.gettext
